25dc9a5b28830b49278add8448d487fdb5da1c50
miguelfiais <miguel.fiais10@gmail.com>
2023-11-13 17:59:36 -0300

    chore: configure color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'bg-price': 'var(--bg-price)',
+        'sold-out': 'var(--sold-out)',
+        popular: 'var(--popular)',
+        titles: 'var(--titles)',
+        'bg-body': 'var(--bg-body)',
+        'bg-secondary': 'var(--bg-secondary)',
+        'text-description': 'var(--text-description)',
       },
     },
   },
