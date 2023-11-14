2bef948addafc94c44b9270fe31b29ba556db95a
miguelfiais <miguel.fiais10@gmail.com>
2023-11-14 10:18:31 -0300

    chore: add supabase domain to next.config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'csyxkpbavpcrhwqhcpyy.supabase.co',
+        port: '',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
